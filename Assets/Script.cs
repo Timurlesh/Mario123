5e544c54d1aadeefd91c524b968be897133936cc
Timurlesh <timyrlesh@gmail.com>
2023-10-14 15:33:25 +0300

    move

--- Assets/Script.cs
+++ Assets/Script.cs
@@ -6,32 +6,39 @@ public class Script : MonoBehaviour
 {
     // Start is called before the first frame update
     public float speed = 10f;
-    private rigidbody2D rb;
+    private Rigidbody2D rb;
     private float horizontal;
-    private bool flipRight = true;
-
-    public float horizontal { get => horizontal; set => horizontal = value; }
+    private bool flip = true;
+    public Animator animator;
 
     void Start()
     {
-        rb = GetComponent<rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
         
 
     // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxis("horizontal");
-        rb.velocity = new Vector2(horizontal,rb.velocity.y);
-        if (horizontal > 0 && !flipRight)
+
+        horizontal = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(horizontal * speed,rb.velocity.y);
+        animator.SetFloat("moveX", Mathf.Abs(horizontal));
+/*      if (horizontal > 0 && !flipRight)
         {
             Flip();
-        }
-        else if (horizontal < 0 && flipRight)
+        }else if (horizontal < 0 && flipRight)
         {
             Flip();
+        }*/
+        if((horizontal > 0 && !flip) || (horizontal < 0 && flip))
+        {
+            transform.localScale *= new Vector2(-1, 1);
+            flip = !flip;
         }
     }
+    /*
     void Flip()
     {
         flipRight = !flipRight;
@@ -39,5 +46,6 @@ public class Script : MonoBehaviour
         theScale.x = theScale.x * (-1);
         transform.localScale = theScale;
     }
+    */
 }
 
